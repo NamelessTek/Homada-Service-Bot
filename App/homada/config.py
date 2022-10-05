467779561edc9b11a0fa4d829308effcd95da17e
luisced <luis.cedillo.maldonado@gmail.com>
2022-10-05 12:28:18 -0500

    Removed secret variables

--- App/homada/config.py
+++ App/homada/config.py
@@ -2,14 +2,12 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get(
-        "SECRET_KEY", '5791628bb0b13ce0c676dfde280ba245')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "SQLALCHEMY_DATABASE_URI", 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
     MYSQL_HOST = os.environ.get("MYSQL_HOST", 'db')
     MYSQL_USER = os.environ.get("MYSQL_USER", 'generic_operator')
-    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", 'example')
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", 'homada')
     MYSQL_DB = os.environ.get("MYSQL_DB", 'homada_db')
     MYSQL_PORT = os.environ.get("MYSQL_PORT", '3306')
     SQLALCHEMY_DATABASE_URI = "mysql://" + MYSQL_USER + ":" + \
