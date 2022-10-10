27cbdadc41997e44d5193cca8eaf0d46f56e6438
luisced <luis.cedillo.maldonado@gmail.com>
2022-10-09 21:48:15 -0500

    Added models for options and ubiacaciones and dataclasses

--- App/homada/models.py
+++ App/homada/models.py
@@ -6,9 +6,12 @@ from datetime import datetime
 
 @dataclass
 class Ubicacion(db.Model):
+    '''
+    Ubicacion Model for storing ubicacion related details 
+    '''
     __tablename__ = 'Ubicacion'
 
-    Id: int = db.Column(db.Integer, primary_key=True,
+    id: int = db.Column(db.Integer, primary_key=True,
                         autoincrement=True, nullable=False)
     ubicacion: str = db.Column(db.String(280), nullable=False)
     url: str = db.Column(db.String(500), nullable=False)
@@ -25,16 +28,19 @@ class Ubicacion(db.Model):
         db.TIMESTAMP, nullable=False, default=datetime.now, onupdate=datetime.now)
 
     def __repr__(self):
-        rows = {"ID": self.Id, "Ubicacion": self.ubicacion, "URL": self.url, "Direccion": self.direccion, "SSID": self.ssid, "Clave": self.clave, "Modem": self.modem,
+        rows = {"ID": self.id, "Ubicacion": self.ubicacion, "URL": self.url, "Direccion": self.direccion, "SSID": self.ssid, "Clave": self.clave, "Modem": self.modem,
                 "Mascotas": self.mascotas, "Status": self.status, "Option": self.option, "Creation Date": self.creation_date, "Last Update": self.last_update}
         return rows
 
 
 @dataclass
 class Option(db.Model):
+    '''
+    Option Model serves as a table to store options and values for the app
+    '''
     __tablename__ = 'Option'
 
-    Id: int = db.Column(db.Integer, primary_key=True)
+    id: int = db.Column(db.Integer, primary_key=True)
     option: int = db. Column(db.Integer, nullable=False)
     value: str = db.Column(db.String(280), nullable=False)
     creation_date: str = db.Column(
@@ -43,6 +49,6 @@ class Option(db.Model):
         db.TIMESTAMP, nullable=False, default=datetime.now, onupdate=datetime.now)
 
     def __repr__(self):
-        rows = {"ID": self.Id, "Option": self.option, "Value": self.value,
+        rows = {"ID": self.id, "Option": self.option, "Value": self.value,
                 "Creation Date": self.creation_date, "Last Update": self.last_update}
         return rows
