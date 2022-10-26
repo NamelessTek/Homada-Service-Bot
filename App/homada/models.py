08605c692ec95cc413cd564f8c9ddf936c9ef46d
Raul Gómez <gomezrbz@gmail.com>
2022-10-25 21:41:23 -0500

    Correction Client email

--- App/homada/models.py
+++ App/homada/models.py
@@ -19,7 +19,6 @@ class Ubicacion(db.Model):
     ssid: str = db.Column(db.String(280), nullable=True)
     clave: str = db.Column(db.String(280), nullable=True)
     modem: str = db.Column(db.String(280), nullable=True)
-    email: str = db.Column(db.String(280), nullable=True)
     mascotas: bool = db.Column(db.Boolean, nullable=False)
     status: bool = db.Column(db.Boolean, nullable=False)
     option: str = db.Column(db.String(280), nullable=True)
@@ -29,7 +28,7 @@ class Ubicacion(db.Model):
         db.TIMESTAMP, nullable=False, default=datetime.now, onupdate=datetime.now)
 
     def __repr__(self):
-        return {"ID": self.id, "Ubicacion": self.ubicacion, "Email": self.email, "URL": self.url, "Direccion": self.direccion, "SSID": self.ssid, "Clave": self.clave, "Modem": self.modem,
+        return {"ID": self.id, "Ubicacion": self.ubicacion, "URL": self.url, "Direccion": self.direccion, "SSID": self.ssid, "Clave": self.clave, "Modem": self.modem,
                 "Mascotas": self.mascotas, "Status": self.status, "Option": self.option, "Creation Date": self.creation_date, "Last Update": self.last_update}
 
 
@@ -66,6 +65,7 @@ class Client(db.Model):
     last_name: str = db.Column(db.String(280), nullable=False)
     phone: str = db.Column(db.String(280), nullable=False)
     status = db.Column(db.Boolean, nullable=False)
+    email: str = db.Column(db.String(280), nullable=True)
     creation_date: str = db.Column(
         db.DateTime, nullable=False, default=datetime.now)
     last_update: str = db.Column(
@@ -76,7 +76,7 @@ class Client(db.Model):
                                   backref=db.backref('client_ubicacion', lazy='dynamic'))
 
     def __repr__(self):
-        return {"ID": self.id, "Name": self.name, "Last Name": self.last_name, "Phone": self.phone, "Status": self.status,
+        return {"ID": self.id, "Name": self.name, "Last Name": self.last_name,"Email": self.email, "Phone": self.phone, "Status": self.status,
                 "Creation Date": self.creation_date, "Last Update": self.last_update}
 
 
