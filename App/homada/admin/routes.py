6f457277aaf527f4c2b6cc6f00973f78879fa006
Raul Gómez <gomezrbz@gmail.com>
2022-12-13 17:30:53 -0600

    Readyz

--- App/homada/admin/routes.py
+++ App/homada/admin/routes.py
@@ -1,16 +1,18 @@
 from flask import Blueprint, request, jsonify, render_template
 from homada.models import *
+import socket
+import datetime
+from uptime import uptime
 
 admin_homada = Blueprint('admin_homada', __name__)
 
 
-@admin_homada.route('/', methods=['GET', 'POST'])
-def status():
-    # get status of the app
-    message = {
-        'status': 'ok',
-        'message': 'App is running',
-        'port': 5555,
-        'app': 'homada',
-    }
-    return jsonify(message), 200
+@admin_homada.route('/readyz', methods=['GET', 'POST'])
+def readyz():
+    data = {}
+    data['status']="oks"
+    data['hostname']=socket.gethostname()
+    data['uptime']=uptime()
+    data['date']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    resp = jsonify(data)
+    return resp
